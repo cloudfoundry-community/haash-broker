23e5c0e3cb361ce8a39cd0bfaa47782d4373fb15
krujos <jkruck@pivotal.io>
2015-09-25 22:28:21 -0500

    update plan ids to use time as well

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
 insert into services (id, name, description, bindable) values (LOCALTIME, LOCALTIME, 'HaaSh - HashMap as a Service', true);
-insert into plans (id, name, description, service_id) values ('1', 'basic', 'Basic Plan', LOCALTIME);
+insert into plans (id, name, description, service_id) values (LOCALTIME, 'basic', 'Basic Plan', LOCALTIME);
